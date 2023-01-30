435b2647eb9387f7d2415cd0df6c9b5d3de52b11
DeeJung <dipesh@fusemachines.com>
2023-01-31 00:11:37 +0545

    update-config

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'DeeJung',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'deejung-nextjs-notion-39etrriza-dipzz1394.vercel.app',
   author: 'Dipesh Jung Pandey',
 
   // open graph metadata (optional)
